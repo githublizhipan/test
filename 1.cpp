02aed843737a2afcff957dcf1b8efbb156596500
githublizhipan <738375827@qq.com>
2020-09-15 15:38:16 +0800

    test

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author: lzp
+	> Mail: 
+	> Created Time: Tue Sep 15 15:37:45 2020
+	> Source: 
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "test" << endl;
+
+    return 0;
+}
